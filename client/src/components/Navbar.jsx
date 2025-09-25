0c6683789f582673e2b2a7de28a09b1461b0bd02
Owais Farooqui <owais@example.com>
2025-09-25 07:14:21 +0530

    last one before push

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,3 +2,5 @@
 console.log('Commit #3: minor fixes in routes');
 // fixed random issues
 console.log('Commit #18: fixed random issues');
+// last one before push
+console.log('Commit #25: last one before push');
