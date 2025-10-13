adafa44c73d65b1e4db6d6a09c255e2bbe2cd552
Owais Farooqui <owais@example.com>
2025-10-13 12:14:05 +0530

    small styling changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,3 +2,5 @@
 console.log('Commit #8: tweaked components');
 // small code cleanup
 console.log('Commit #9: small code cleanup');
+// small styling changes
+console.log('Commit #14: small styling changes');
