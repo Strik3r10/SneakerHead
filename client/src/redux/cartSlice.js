f8a46abdbab9c43d93b041aa3a6f04f938b419aa
Owais Farooqui <owais@example.com>
2025-10-27 03:14:16 +0530

    readme and comments update

--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -2,3 +2,5 @@
 console.log('Commit #1: added basic server files');
 // added few test endpoints
 console.log('Commit #5: added few test endpoints');
+// readme and comments update
+console.log('Commit #22: readme and comments update');
